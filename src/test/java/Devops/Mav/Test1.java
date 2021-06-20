c0339b291fa5ae1941bd3a11cbc812933598db67
Biswajit <biswajit13dah@gmail.com>
2021-06-20 17:45:29 -0400

    Files added

---
+++ src/test/java/Devops/Mav/Test1.java
@@ -0,0 +1,12 @@
+package Devops.Mav;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+
+	@Test
+	public void login() {
+		System.out.println("Logged In");
+	}
+
+}
---
+++ src/test/java/Devops/Mav/Test2.java
@@ -0,0 +1,19 @@
+package Devops.Mav;
+
+import org.testng.annotations.Test;
+
+public class Test2 {
+
+	@Test
+	public void logout() {
+		System.out.println("Logged Out");
+	}
+	@Test
+	public void browser() {
+		System.out.println("Browser In");
+	}
+	@Test
+	public void payment() {
+		System.out.println("Payment");
+	}
+}
